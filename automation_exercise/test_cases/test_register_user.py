9c8d7d3178cea0f8d80d2576586587130213710d
Tomasz Nowakowski <toalak@gmail.com>
2024-12-08 23:16:12 +0100

    Moved user credentials to separate file

--- automation_exercise/test_cases/test_register_user.py
+++ automation_exercise/test_cases/test_register_user.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
+#from ..credentials import *
+from .. import credentials as c
+
 from playwright.sync_api import Page, expect
-
-
-USER_NAME = "Ved"
-PASSWORD = "111"
 
 
 def test_register_user(page: Page):
@@ -19,47 +18,47 @@ def test_register_user(page: Page):
     expect(page.get_by_role("heading", name="New User Signup!")).to_be_visible()
 
     page.get_by_placeholder("Name").click()
-    page.get_by_placeholder("Name").fill(USER_NAME)
+    page.get_by_placeholder("Name").fill(c.USER_NAME)
     page.locator("form").filter(has_text="Signup").get_by_placeholder("Email Address").click()
-    page.locator("form").filter(has_text="Signup").get_by_placeholder("Email Address").fill("ved@example.com")
+    page.locator("form").filter(has_text="Signup").get_by_placeholder("Email Address").fill(c.EMAIL_ADDRESS)
     page.get_by_role("button").filter(has_text="Signup").click()
 
     expect(page.get_by_text("Enter Account Information")).to_be_visible()
     page.get_by_label("Mr.").check()
-    expect(page.get_by_label("Name *", exact=True)).to_have_value(USER_NAME)
-    expect(page.get_by_label("Email *", exact=True)).to_have_value("ved@example.com")
+    expect(page.get_by_label("Name *", exact=True)).to_have_value(c.USER_NAME)
+    expect(page.get_by_label("Email *", exact=True)).to_have_value(c.EMAIL_ADDRESS)
     page.get_by_label("Password *").click()
-    page.get_by_label("Password *").fill(PASSWORD)
-    page.locator("#days").select_option("1")
-    page.locator("#months").select_option("1")
-    page.locator("#years").select_option("2021")
+    page.get_by_label("Password *").fill(c.PASSWORD)
+    page.locator("#days").select_option(c.BIRTHDAY_DAY)
+    page.locator("#months").select_option(c.BIRTHDAY_MONTH)
+    page.locator("#years").select_option(c.BIRTHDAY_YEAR)
     page.get_by_label("Sign up for our newsletter!").check()
     page.get_by_label("Receive special offers from").check()
     page.get_by_label("First name *").click()
-    page.get_by_label("First name *").fill("Ved")
+    page.get_by_label("First name *").fill(c.FIRST_NAME)
     page.get_by_label("Last name *").click()
-    page.get_by_label("Last name *").fill("Vid")
+    page.get_by_label("Last name *").fill(c.LAST_NAME)
     page.get_by_label("Company", exact=True).click()
-    page.get_by_label("Company", exact=True).fill("N/A")
+    page.get_by_label("Company", exact=True).fill(c.COMPANY)
     page.get_by_label("Address *").click()
-    page.get_by_label("Address *").fill("Main Street")
+    page.get_by_label("Address *").fill(c.ADDRESS)
     page.get_by_label("Address 2").click()
-    page.get_by_label("Address 2").fill("123B")
-    page.get_by_label("Country *").select_option("United States")
+    page.get_by_label("Address 2").fill(c.ADDRESS_2)
+    page.get_by_label("Country *").select_option(c.COUNTRY)
     page.get_by_label("State *").click()
-    page.get_by_label("State *").fill("NY")
+    page.get_by_label("State *").fill(c.STATE)
     page.get_by_label("City *").click()
-    page.get_by_label("City *").fill("New York")
+    page.get_by_label("City *").fill(c.CITY)
     page.locator("#zipcode").click()
-    page.locator("#zipcode").fill("111-11")
+    page.locator("#zipcode").fill(c.ZIPCODE)
     page.get_by_label("Mobile Number *").click()
-    page.get_by_label("Mobile Number *").fill("111-111-111")
+    page.get_by_label("Mobile Number *").fill(c.MOBILE)
     page.get_by_role("button", name="Create Account").click()
 
     expect(page.get_by_text("Account Created")).to_be_visible()
     page.get_by_role("link", name="Continue").click()
 
-    expect(page.get_by_text(f"Logged in as {USER_NAME}")).to_be_visible()
+    expect(page.get_by_text(f"Logged in as {c.USER_NAME}")).to_be_visible()
     page.locator("li").filter(has_text="Delete Account").click()
 
     expect(page.get_by_text("Account Deleted")).to_be_visible()
