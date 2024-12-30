5cc0114b9d6b1ebe42dbe6d57cfe849c7648ba65
Tomasz Nowakowski <toalak@gmail.com>
2024-12-30 19:51:39 +0100

    Fifth test case from https://automationexercise.com/ covered – Register User With Existing Email

---
+++ automation_exercise/test_cases/test_case_5_register_with_existing_email.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+from ..create_user import create_user
+from ..delete_user import delete_user
+
+from playwright.sync_api import Page, expect
+
+
+def setup_function():
+    delete_user()
+    create_user()
+
+
+def test_register_user_with_existing_email(page: Page):
+    page.goto("https://www.automationexercise.com/")
+    page.get_by_label("Consent", exact=True).click()
+    expect(page.locator("body")).to_be_visible()
+    page.locator("li").filter(has_text="Signup / Login").click()
+    expect(page.get_by_role("heading", name="New User Signup!")).to_be_visible()
+    page.get_by_placeholder("Name").click()
+    page.get_by_placeholder("Name").fill("Ved")
+    page.locator("form").filter(has_text="Signup").get_by_placeholder("Email Address").click()
+    page.locator("form").filter(has_text="Signup").get_by_placeholder("Email Address").fill("ved@example.com")
+    page.get_by_role("button", name="Signup").click()
+    expect(page.get_by_text("Email Address already exist!")).to_be_visible()
+
+
+def teardown_function():
+    delete_user()
