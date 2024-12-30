3d6316650be580055bc4670befbfeb6b204eb703
Tomasz Nowakowski <toalak@gmail.com>
2024-12-30 21:52:13 +0100

    I was unable to finish sixth case from https://automationexercise.com/ Contact Us Form as click on Submit button is not recognized and instead the form content is being erased

---
+++ automation_exercise/test_cases/test_case_6__bugged_contact_us.py
@@ -0,0 +1,35 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.automationexercise.com/")
+    page.get_by_label("Consent", exact=True).click()
+    page.locator("li").filter(has_text="Contact us").click()
+    page.get_by_placeholder("Name").click()
+    page.get_by_placeholder("Name").fill("Ved")
+    page.get_by_placeholder("Email", exact=True).click()
+    page.get_by_placeholder("Email", exact=True).fill("ved@example.com")
+    page.get_by_placeholder("Subject").click()
+    page.get_by_placeholder("Subject").fill("Test subject")
+    page.get_by_placeholder("Your Message Here").click()
+    page.get_by_placeholder("Your Message Here").fill("Test message")
+    with page.expect_file_chooser() as fc_info:
+        page.locator("input[name=\"upload_file\"]").click()
+        file_chooser = fc_info.value
+        file_chooser.set_files("test_file_for_contact_us.txt")
+    page.once("dialog", lambda dialog: dialog.dismiss())
+    page.get_by_role("button", name="Submit").click()
+    expect(page.locator("#contact-page").get_by_text("Success! Your details have")).to_be_visible()
+    page.get_by_role("link", name=" Home").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
