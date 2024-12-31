da356f0b755af8aff15f47e01615d0f8eacddcf2
Tomasz Nowakowski <toalak@gmail.com>
2024-12-31 01:25:36 +0100

    Seventh test case from https://automationexercise.com/ covered – Visit Test Cases

---
+++ automation_exercise/test_cases/test_case_7_test_cases.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_example(page: Page) -> None:
+    page.goto("https://www.automationexercise.com/")
+    page.get_by_label("Consent", exact=True).click()
+    expect(page.locator("body")).to_be_visible()
+    page.locator("li").filter(has_text="Test Cases").click()
+    expect(page).to_have_url(re.compile("automationexercise.com/test_cases"))
