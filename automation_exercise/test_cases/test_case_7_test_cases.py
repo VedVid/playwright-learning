3fb52144c4855cb989ea8a15a99448154161afaf
Tomasz Nowakowski <toalak@gmail.com>
2025-01-02 17:04:25 +0100

    Tiny changes to 7th and 8th test cases – renamed test functions and removed unnecessary printing

--- automation_exercise/test_cases/test_case_7_test_cases.py
+++ automation_exercise/test_cases/test_case_7_test_cases.py
@@ -5,7 +5,7 @@ import re
 from playwright.sync_api import Page, expect
 
 
-def test_example(page: Page) -> None:
+def test_visit_test_cases_page(page: Page) -> None:
     page.goto("https://www.automationexercise.com/")
     page.get_by_label("Consent", exact=True).click()
     expect(page.locator("body")).to_be_visible()
--- automation_exercise/test_cases/test_case_8_verify_all_producs.py
+++ automation_exercise/test_cases/test_case_8_verify_all_producs.py
@@ -4,7 +4,7 @@
 from playwright.sync_api import Page, expect
 
 
-def test_example(page: Page) -> None:
+def test_verify_all_products(page: Page) -> None:
     page.goto("https://automationexercise.com/")
     page.get_by_label("Consent", exact=True).click()
     expect(page.locator("body")).to_be_visible()
@@ -25,21 +25,17 @@ def test_example(page: Page) -> None:
     category = page.get_by_text("Category:")
     expect(category).to_be_visible()
     assert(len(category.text_content()) > len("Category: "))
-    print(category.text_content())
 
     price = page.get_by_text("Rs.")
     expect(price).to_be_visible()
     assert(price.text_content()[-1].isnumeric())
-    print(price.text_content())
 
     availability = condition = page.locator(".product-information > p:nth-child(6)")
     expect(availability).to_contain_text("Availability")
     expect(availability).to_be_visible()
-    print(availability.text_content())
     expect(availability).to_contain_text("tock")
 
     condition = page.locator(".product-information > p:nth-child(7)")
     expect(condition).to_contain_text("Condition")
     expect(condition).to_be_visible()
-    print(condition.text_content())
     assert(len(condition.text_content()) > len("Condition: "))
