3dd1ba88927809eb730426710f763c8618a53b7f
Tomasz Nowakowski <toalak@gmail.com>
2025-09-23 14:03:47 +0200

    15th test case from https://automationexercise/ covered -- Place Order: Register before Checkout This commit also adds separete "actions.py" file that will be used aggregate functions that are used by multiple tests

---
+++ automation_exercise/test_cases/test_case_15_place_order_register_before_checkout.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+
+from .. import credentials as c
+from .. import actions as a
+
+from playwright.sync_api import Page, expect
+
+
+def test_place_order_register_before_checkout(page: Page) -> None:
+    a.goto_page(page)
+
+    page.locator("li").filter(has_text="Signup / Login").click()
+    page.get_by_placeholder("Name").click()
+    page.get_by_placeholder("Name").fill(c.USER_NAME)
+    page.locator("form").filter(has_text="Signup").get_by_placeholder("Email Address").click()
+    page.locator("form").filter(has_text="Signup").get_by_placeholder("Email Address").fill(c.EMAIL_ADDRESS)
+    page.get_by_role("button", name="Signup").click()
+
+    a.new_user_form_fill_and_confirm(page)
+    expect(page.get_by_text("Account Created!")).to_be_visible()
+    page.get_by_role("link", name="Continue").click()
+
+    page.locator(".productinfo > .btn").first.click()
+    page.get_by_role("link", name="View Cart").click()
+
+    expect(page.get_by_text("Shopping Cart")).to_be_visible()
+    page.get_by_text("Proceed To Checkout").click()
+
+    expect(page.get_by_text("Rs.").nth(1)).to_be_visible()
+    first_product_css = ".cart_price > p:nth-child(1)"
+    first_product_price = page.locator(first_product_css).text_content()
+    assert(first_product_price == page.get_by_text("Rs.").first.text_content())
+    total_price_css = ".table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(4) > p:nth-child(1)"
+    total_price = page.locator(total_price_css).text_content()
+    assert (first_product_price == total_price)
+    expect(page.get_by_role("heading", name="Your delivery address")).to_be_visible()
+    page.locator("textarea[name=\"message\"]").click()
+    page.locator("textarea[name=\"message\"]").fill("test")
+    page.get_by_role("link", name="Place Order").click()
+
+    a.handle_card_payment(page)
+
+    page.locator("li").filter(has_text="Delete Account").click()
+    expect(page.get_by_text("Account Deleted!")).to_be_visible()
+    page.get_by_role("link", name="Continue").click()
