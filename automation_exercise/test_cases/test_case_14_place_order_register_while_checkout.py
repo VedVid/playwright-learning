f188876fa0ca7dc897aa4ed905cf9153a943d027
Tomasz Nowakowski <toalak@gmail.com>
2025-09-23 12:40:17 +0200

    Improvements to 14 test case Filling form is handled by a separate function; changed test name

--- automation_exercise/test_cases/test_case_14_place_order_register_while_checkout.py
+++ automation_exercise/test_cases/test_case_14_place_order_register_while_checkout.py
@@ -2,6 +2,7 @@
 
 
 from .. import credentials as c
+from ..fill_new_user_form import fill_and_confirm as fill_form
 from ..delete_user import delete_user
 
 from playwright.sync_api import Page, expect
@@ -11,7 +12,7 @@ def setup_function():
     delete_user()
 
 
-def test_example(page: Page) -> None:
+def test_place_order_and_register(page: Page) -> None:
     page.goto("https://www.automationexercise.com/")
     page.get_by_label("Consent", exact=True).click()
     expect(page.locator("body")).to_be_visible()
@@ -29,37 +30,7 @@ def test_example(page: Page) -> None:
     page.locator("form").filter(has_text="Signup").get_by_placeholder("Email Address").fill(c.EMAIL_ADDRESS)
     page.get_by_role("button", name="Signup").click()
 
-    expect(page.get_by_text("Enter Account Information")).to_be_visible()
-    page.get_by_label(c.TITLE).check()
-    expect(page.get_by_label("Name *", exact=True)).to_have_value(c.USER_NAME)
-    expect(page.get_by_label("Email *", exact=True)).to_have_value(c.EMAIL_ADDRESS)
-    page.get_by_label("Password *").click()
-    page.get_by_label("Password *").fill(c.PASSWORD)
-    page.locator("#days").select_option(c.BIRTHDAY_DAY)
-    page.locator("#months").select_option(c.BIRTHDAY_MONTH)
-    page.locator("#years").select_option(c.BIRTHDAY_YEAR)
-    page.get_by_label("Sign up for our newsletter!").check()
-    page.get_by_label("Receive special offers from").check()
-    page.get_by_label("First name *").click()
-    page.get_by_label("First name *").fill(c.FIRST_NAME)
-    page.get_by_label("Last name *").click()
-    page.get_by_label("Last name *").fill(c.LAST_NAME)
-    page.get_by_label("Company", exact=True).click()
-    page.get_by_label("Company", exact=True).fill(c.COMPANY)
-    page.get_by_label("Address *").click()
-    page.get_by_label("Address *").fill(c.ADDRESS)
-    page.get_by_label("Address 2").click()
-    page.get_by_label("Address 2").fill(c.ADDRESS_2)
-    page.get_by_label("Country *").select_option(c.COUNTRY)
-    page.get_by_label("State *").click()
-    page.get_by_label("State *").fill(c.STATE)
-    page.get_by_label("City *").click()
-    page.get_by_label("City *").fill(c.CITY)
-    page.locator("#zipcode").click()
-    page.locator("#zipcode").fill(c.ZIPCODE)
-    page.get_by_label("Mobile Number *").click()
-    page.get_by_label("Mobile Number *").fill(c.MOBILE)
-    page.get_by_role("button", name="Create Account").click()
+    fill_form(page)
 
     expect(page.get_by_text("Account Created!")).to_be_visible()
     page.get_by_role("link", name="Continue").click()
